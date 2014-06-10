ef3bf3bb931ef0c30a0bcddb28d8385b4537d0bb
taylormonacelli <tailor@u.washington.edu>
2014-06-09 19:25:01 -0700

    Drop repo from 18MB down to 4MB

---
+++ Makefile
@@ -0,0 +1,4 @@
+t1:
+	sh -x remove-large-files.sh
+clean:
+	rm -f banned.txt
--- remove-large-files.sh
+++ remove-large-files.sh
@@ -1,28 +1,39 @@
 #/bin/sh
 
+bannedlist=$(pwd)/banned.txt
 
-rm -rf /tmp/nsis-streambox3
-rm -rf /tmp/nsis-streambox3b
-rm -rf /tmp/nsis-streambox3a
 rm -rf /tmp/nsis-streambox3.git
+rm -rf /tmp/nsis-streambox3
+rm -rf /tmp/nsis-streambox3c
 
-git clone --mirror ~/pdev/nsis-streambox2 /tmp/nsis-streambox3a
-git clone /tmp/nsis-streambox3a /tmp/nsis-streambox3b
+git clone --mirror ~/pdev/nsis-streambox2 /tmp/nsis-streambox3.git
+git clone /tmp/nsis-streambox3.git /tmp/nsis-streambox3
 
-cd /tmp/nsis-streambox3b && git fetch --all
+cd /tmp/nsis-streambox3
+
+git config -l | grep -i url
 
 rm -f autoit-v3-setup.exe
 rm -f nsis-2.46-Unicode-setup.exe
 rm -f nsis-2.46-setup.exe
 rm -f sed-4.2-1-bin.zip
 rm -f sed-4.2-1-dep.zip
+rm -f 7z920.msi
 
 git commit -am "Deleting large files"
+git push
 
-echo ***REMOVED*** >banned.txt
-exit 1
+echo ***REMOVED*** >$bannedlist
+
 java \
     -jar /Users/demo/Downloads/bfg-1.11.6.jar \
     --strip-blobs-bigger-than 1M \
-    --replace-text banned.txt \
+    --replace-text $bannedlist \
     /tmp/nsis-streambox3.git
+
+cd /tmp/nsis-streambox3.git
+git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+
+du -sh ~/pdev/nsis-streambox2
+du -sh /tmp/nsis-streambox3.git
