c272f9b1e505c2c96521c1e33d1ba43b44c265f3
taylormonacelli <tailor@u.washington.edu>
2015-02-21 22:31:58 -0800

    Stop repeating myself so I don't forget files

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ bfg-repo-cleaner-practice
 BFG: http://rtyley.github.io/bfg-repo-cleaner
 
 Practice using BFG to clean git repos.
+
+BFG is an alternative to git-filter-branch.
--- remove-large-files.sh
+++ remove-large-files.sh
@@ -9,6 +9,8 @@ wget --timestamping http://repo1.maven.org/maven2/com/madgag/bfg/$bfgver/$bfgjar
 
 bannedlist=$(pwd)/banned.txt
 
+rm -rf /tmp/$0.tmp
+
 rm -rf /tmp/nsis-streambox3.git
 rm -rf /tmp/nsis-streambox3
 rm -rf /tmp/nsis-streambox3b
@@ -20,22 +22,32 @@ cd /tmp/nsis-streambox3
 
 git config -l | grep -i url
 
-rm -f autoit-v3-setup.exe
-rm -f nsis-2.46-Unicode-setup.exe
-rm -f nsis-2.46-setup.exe
-rm -f sed-4.2-1-bin.zip
-rm -f sed-4.2-1-dep.zip
-rm -f 7z920.msi
+cat << __EOT__ >>/tmp/$0.tmp
+regedit.exe
+7za.exe
+autoit-v3-setup.exe
+nsis-2.46-Unicode-setup.exe
+nsis-2.46-setup.exe
+sed-4.2-1-bin.zip
+sed-4.2-1-dep.zip
+7z920.msi
+__EOT__
+
+cat /tmp/$0.tmp | sort | while read f; do rm -f $f; done;
 
 git commit -am "Deleting large files"
 git push
+
+bfg_delete_files_args="{$(cat /tmp/$0.tmp | sort | tr '\n' ',' | sed -e 's,.$,,')}"
+
+# echo $bfg_delete_files_args
 
 echo ***REMOVED*** >$bannedlist
 
 java \
     -jar $bfgjarPath \
     --replace-text $bannedlist \
-    --delete-files '{autoit-v3-setup.exe,nsis-2.46-Unicode-setup.exe,nsis-2.46-setup.exe,sed-4.2-1-bin.zip,sed-4.2-1-dep.zip,7z920.msi}' \
+    --delete-files "$bfg_delete_files_args" \
     /tmp/nsis-streambox3.git
 
 cd /tmp/nsis-streambox3.git
