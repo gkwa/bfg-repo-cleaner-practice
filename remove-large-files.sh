b535e227c4ff8ef5e928ab70661b0501aeaf1f97
Taylor Monacelli <taylormonacelli@gmail.com>
2015-04-10 19:56:51 -0700

    yada-yada

--- remove-large-files.sh
+++ remove-large-files.sh
@@ -26,11 +26,11 @@ rm -rf mysql_win_installer
 rm -rf mysql_win_installerb
 
 git clone --bare ~/pdev/streambox/mysql_win_installer mysql_win_installer.git
-# git clone --bare git@gitlab.com:mysql_win_installer.git mysql_win_installer.git
+# git clone --bare git@gitlab.com:streambox/mysql_win_installer.git mysql_win_installer.git
 (cd mysql_win_installer.git
  git remote rm origin
  git remote
- git remote add origin git@gitlab.com:mysql_win_installer.git
+ git remote add origin git@gitlab.com:streambox/mysql_win_installer.git
 )
 git clone mysql_win_installer.git mysql_win_installer
 
